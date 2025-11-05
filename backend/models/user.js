0e5d3b1345ad136cff13b3942544a4ec2ac2ae68
Tedla1808 <tedlaworku1@gmail.com>
2025-11-05 22:52:18 +0300

    Initial commit of complete project

--- backend/models/user.js
+++ backend/models/user.js
@@ -19,12 +19,8 @@ const userSchema = new mongoose.Schema({
         dailySummaryEnabled: { type: Boolean, default: false },
         dailySummaryTime: { type: String, default: '08:00' },
         timezone: { type: String, default: 'UTC' },
-        lastAdvancedReminderDate: { type: String, default: null },
-        
-        // --- MODIFIED ---
-        calendarType: { type: String, default: 'gregorian' },
-        // --- ADDED ---
-        hasChosenCalendar: { type: Boolean, default: false } 
+        lastAdvancedReminderDate: { type: String, default: null }
+        // The calendarType and hasChosenCalendar fields have been removed.
     }
 });
 
